22548763b2eec0ef4a6f81dc20a5d91cc81f87af
radinbhh <radin.baharsefat@gmail.com>
2025-10-25 18:25:31 +0330

    .

---
+++ pattern_count.py
@@ -0,0 +1,28 @@
+def pattern_count(string, pattern):
+        count = 0
+        for i in range(0, len(string) - len(pattern)):
+            if (string[i:i + len(pattern)] == pattern):
+                count+=1
+
+        return count
+
+
+def frequent_words(string, k):
+    frequent_patterns = []
+    count = []
+    max_count = -1
+    for i in range(0,len(string) - k):
+        pattern = string[i:i+k]
+        count.append(pattern_count(string,pattern))
+        if (count[i] > max_count):
+           max_count = count[i]
+    
+    for i in range(0,len(string) - k):
+        if (count[i] == max_count):
+            frequent_patterns.append(string[i:i + k])
+          
+    frequent_patterns = list(set(frequent_patterns))
+    return frequent_patterns
+string = str(input("Enter the genome: "))
+k = int(input("Enter k: "))
+print(frequent_words(string, k))
